a1fea35aedee759ff5434994fb39116e921071a4
SaadiahSaleem <saadiah.saleem@gmail.com>
2025-08-31 23:20:27 +0200

    Update case_sensitivity.js

--- Understanding-Case-Sensitivity-in-Variables/case_sensitivity.js
+++ Understanding-Case-Sensitivity-in-Variables/case_sensitivity.js
@@ -2,8 +2,16 @@
 var StUdLyCapVaR;
 var properCamelCase;
 var TitleCaseOver;
+var STUDLYCAPVAR;
+var PRoperCAmelCAse;
+var tITLEcASEoVER;
+var studlyCapVar;
+var titleCaseOver;
 
 // Variable assignments
 STUDLYCAPVAR = 10;
 PRoperCAmelCAse = "A String";
 tITLEcASEoVER = 9000;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
