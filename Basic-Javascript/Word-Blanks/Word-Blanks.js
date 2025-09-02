9bc6099cedadd40f8a804cd4d1363c66e7923adf
saadiah96 <saadiah.saleem@gmail.com>
2025-09-02 18:56:12 +0200

    Word_Blanks

---
+++ Basic-Javascript/Word-Blanks/Word-Blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "A "+ myAdjective + " and a hairy " + myNoun + " came to me but " + myVerb + " really " + myAdverb; // Change this line
+// Only change code above this line
