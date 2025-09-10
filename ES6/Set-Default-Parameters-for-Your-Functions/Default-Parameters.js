a03cd2990b36b4a50f0cb408044a8b19b753636e
saadiah96 <saadiah.saleem@gmail.com>
2025-09-11 00:09:48 +0200

    Default-Parameters

---
+++ ES6/Set-Default-Parameters-for-Your-Functions/Default-Parameters.js
@@ -0,0 +1,6 @@
+// Only change code below this line
+const increment = (number = 1 ,value = 1) => number + value;
+// const increment = (number, value) => number + value;
+// Only change code above this line
+console.log(increment(5,2));
+console.log(increment(5));
