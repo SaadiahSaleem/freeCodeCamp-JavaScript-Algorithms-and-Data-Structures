9a9077a5fbdeeef2d92b16da4a7ca55ead8eff41
saadiah96 <saadiah.saleem@gmail.com>
2025-09-21 19:28:25 +0200

    Array's Contents-Using-Bracket-Notation

---
+++ Basic-Data-Structures/Access-an-Array's-Contents-Using-Bracket-Notation/Array's Contents-Using-Bracket-Notation.js
@@ -0,0 +1,8 @@
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[0] = "a"
+myArray[1] = "s"
+myArray[2] = "c"
+myArray[3] = "d"
+// Only change code above this line
+console.log(myArray);
