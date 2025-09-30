2d1e6024c42d67548507af23290d3bda9233577b
saadiah96 <saadiah.saleem@gmail.com>
2025-09-30 22:49:54 +0200

    Pass-Arguments

---
+++ Functional-Programming/Pass-Arguments-to-Avoid-External-Dependence-in-a-Function/Pass-Arguments.js
@@ -0,0 +1,10 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(inc) {
+  return inc + 1;
+
+  // Only change code above this line
+}
+console.log(incrementer(fixedValue));
