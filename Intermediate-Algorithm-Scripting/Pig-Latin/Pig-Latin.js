5b9e4a85963092053da2f1aedb128f4aff26af76
saadiah96 <saadiah.saleem@gmail.com>
2025-10-09 17:44:50 +0200

    Pig-Latin

--- Intermediate-Algorithm-Scripting/Pig-Latin/Pig-Latin.js
+++ Intermediate-Algorithm-Scripting/Pig-Latin/Pig-Latin.js
@@ -1,10 +1,14 @@
 function translatePigLatin(str) {
-   if (/^[aeiou]/.test(str)) {
+  if (/^[aeiou]/.test(str)) {
+    // Starts with a vowel
     return str + "way";
   } else {
-    // Find where the first vowel appears
     const vowelIndex = str.search(/[aeiou]/);
-    // Move all consonants before that vowel to the end, and add "ay"
+    if (vowelIndex === -1) {
+      // No vowels found
+      return str + "ay";
+    }
+    // Normal case: move consonants before first vowel
     return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
   }
 }
