c5c9491021966c44351a4f8afa77402be16b4e53
saadiah96 <saadiah.saleem@gmail.com>
2025-10-10 18:07:36 +0200

    Sum-All-Primes

---
+++ Intermediate-Algorithm-Scripting/Sum-All-Primes/Sum-All-Primes.js
@@ -0,0 +1,19 @@
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+function sumPrimes(num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(sumPrimes(10));
